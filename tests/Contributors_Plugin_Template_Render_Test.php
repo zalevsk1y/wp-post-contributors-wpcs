e5b794b431e41a0a34268fea1c1df255d62c50fa
Evgeniy Zalevskiy <2600@ukr.net>
2019-08-26 17:36:44 +0300

    add test wrong templatte path

--- tests/Contributors_Plugin_Template_Render_Test.php
+++ tests/Contributors_Plugin_Template_Render_Test.php
@@ -4,6 +4,11 @@ use Spatie\Snapshots\MatchesSnapshots;
 
 class Contributors_Plugin_Template_Render_Test extends \WP_UnitTestCase{
     use MatchesSnapshots;
+    public function test_wrong_template_path(){
+        $wrong_path='/mock/wrong_file_name.php';
+        $this->expectException(\Exception::class);
+        $template=new Contributors_Plugin_Template_Render($wrong_path);
+    }
     public function test_render(){
         $path=__DIR__.'/mock/test-render-mock.php';
         $template=new Contributors_Plugin_Template_Render($path);
