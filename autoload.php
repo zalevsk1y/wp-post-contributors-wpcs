77fc27d8b893e713afaf53c15f9b5326faaff760
Evgeniy Zalevskiy <2600@ukr.net>
2019-08-28 14:42:17 +0300

    fixed coding style

--- autoload.php
+++ autoload.php
@@ -1,14 +1,23 @@
 <?php
+/**
+ * Autoload function.
+ *
+ * @package Main
+ */
 
-function contributors_plugin_autoload($class)
-{
-    $file_name='class-'.str_replace('_','-',strtolower($class)).'.php';
-    $path=__DIR__ . DIRECTORY_SEPARATOR . 'inc'. DIRECTORY_SEPARATOR .'classes'.DIRECTORY_SEPARATOR .$file_name;
+/**
+ * Autoload function.
+ *
+ * @param string $class Class name.
+ * @return void
+ */
+function contributors_plugin_autoload( $class ) {
+	$file_name = 'class-' . str_replace( '_', '-', strtolower( $class ) ) . '.php';
+	$path      = __DIR__ . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $file_name;
 
- 
-    if (file_exists($path)) {
-        include $path;
-    }
+	if ( file_exists( $path ) ) {
+		include $path;
+	}
 }
 
-spl_autoload_register("contributors_plugin_autoload");
+spl_autoload_register( 'contributors_plugin_autoload' );
--- contributors-plugin.php
+++ contributors-plugin.php
@@ -12,25 +12,25 @@ License: MIT
 <?php
 
 
-if (!defined("ABSPATH")) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
-define("CONTRIBUTORS_PLUGIN_VERSION", "1.0.0");
-define("CONTRIBUTORS_PLUGIN_SLUG", "wp-post-contributors");
-define("CONTRIBUTORS_PLUGIN_NAMESPACE", "ContributorsPlugin");
-define("CONTRIBUTORS_PLUGIN_DIR", plugin_dir_path(__FILE__));
-define("CONTRIBUTORS_PLUGIN_URL", plugins_url("", __FILE__));
-define("CONTRIBUTORS_PLUGIN_NONCE", "wp_contributors_plugin_nonce");
-define("CONTRIBUTORS_PLUGIN_NONCE_ACTION", "wp_contributors_plugin_nonce_action");
-define("CONTRIBUTORS_PLUGIN_INPUT_FIELD", "wp_contributors_plugin_value[]");
-define("CONTRIBUTORS_PLUGIN_FIELD", "wp_contributors_plugin_value");
-define("CONTRIBUTORS_PLUGIN_META", "_wp_contributors_plugin");
+define( 'CONTRIBUTORS_PLUGIN_VERSION', '1.0.0' );
+define( 'CONTRIBUTORS_PLUGIN_SLUG', 'wp-post-contributors' );
+define( 'CONTRIBUTORS_PLUGIN_NAMESPACE', 'ContributorsPlugin' );
+define( 'CONTRIBUTORS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'CONTRIBUTORS_PLUGIN_URL', plugins_url( '', __FILE__ ) );
+define( 'CONTRIBUTORS_PLUGIN_NONCE', 'wp_contributors_plugin_nonce' );
+define( 'CONTRIBUTORS_PLUGIN_NONCE_ACTION', 'wp_contributors_plugin_nonce_action' );
+define( 'CONTRIBUTORS_PLUGIN_INPUT_FIELD', 'wp_contributors_plugin_value[]' );
+define( 'CONTRIBUTORS_PLUGIN_FIELD', 'wp_contributors_plugin_value' );
+define( 'CONTRIBUTORS_PLUGIN_META', '_wp_contributors_plugin' );
 
-require_once "autoload.php";
-$admin_template_path =  CONTRIBUTORS_PLUGIN_DIR . "/templates/contributors-plugin-admin-template.php";
-$post_template_path = apply_filters("contributors_plugin_post_template", CONTRIBUTORS_PLUGIN_DIR . "/templates/contributors-plugin-post-template.php");
-$modules = [];
-$modules["admin_template"] = new Contributors_Plugin_Template_Render($admin_template_path);
-$modules["post_template"] = new Contributors_Plugin_Template_Render($post_template_path);
-$modules["metabox"] = new Contributors_Plugin_Metabox_Controller($modules["admin_template"], $modules["post_template"]);
-$modules["main"] = new Contributors_Plugin_Core_Main();
+require_once 'autoload.php';
+$admin_template_path       = CONTRIBUTORS_PLUGIN_DIR . '/templates/contributors-plugin-admin-template.php';
+$post_template_path        = apply_filters( 'contributors_plugin_post_template', CONTRIBUTORS_PLUGIN_DIR . '/templates/contributors-plugin-post-template.php' );
+$modules                   = [];
+$modules['admin_template'] = new Contributors_Plugin_Template_Render( $admin_template_path );
+$modules['post_template']  = new Contributors_Plugin_Template_Render( $post_template_path );
+$modules['metabox']        = new Contributors_Plugin_Metabox_Controller( $modules['admin_template'], $modules['post_template'] );
+$modules['main']           = new Contributors_Plugin_Core_Main();
